100107d6ddb27eea4b2c651365f5588a39b1d270
dangc <932297885@qq.com>
2021-06-07 23:49:27 +0800

    增加分类左侧tabbar

---
+++ src/components/common/lefttabbar/LeftTabbar.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="left-tabbar">
+    <ul>
+      <li v-for="(item,index) in category"  
+          :key="index" 
+          :class="{active : index === currentIndex,
+                  aboveActive : index === currentIndex-1,
+                  belowActive : index === currentIndex+1
+          }"
+          @click="barclick(index)">
+          <!-- 模仿京东app的分类 在li标签内加一个容器,实现active右上角，右下角圆弧效果 -->
+        <div>
+          {{item.name}}
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LeftTabbar",
+  data(){
+    return{
+      currentIndex:0,
+    }
+  },
+  props: {
+    category: {
+      type: Array
+    }
+  },
+  created () {
+    // console.log(this.category);
+  },
+  computed: {
+
+  },
+  methods: {
+    barclick(index){
+      this.currentIndex = index;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.left-tabbar{
+  overflow:hidden;
+  /* height: 1000px; */
+  margin-top:44px
+}
+.left-tabbar ul{
+  width: 100px;
+  background: #f8f8f8;
+}
+.left-tabbar ul .left-tabbar li {
+  display: block;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  
+}
+.left-tabbar li {
+  height: 46px;
+  line-height: 46px;
+  font-size: 14px;
+  text-align: center;
+  position: relative;
+}
+.aboveActive{
+  background: #fff;
+}
+.aboveActive div{
+  background: #f8f8f8;
+  border-bottom-right-radius: 15px;
+}
+.belowActive{
+  background: #fff;
+}
+.belowActive div{
+  background: #f8f8f8;
+  border-top-right-radius: 15px;
+}
+.active div{
+  background: #fff !important;
+  border-top-left-radius:15px;
+  border-bottom-left-radius:15px
+}
+</style>
--- src/views/category/category.vue
+++ src/views/category/category.vue
@@ -1,15 +1,61 @@
 <template>
   <div>
-    <h2>我是分类</h2>
+    <nav-bar class="categroy-nav">
+      <template v-slot:center>
+        <div>分类查看</div>
+      </template>
+    </nav-bar>
+    <left-tabbar :category="category"></left-tabbar>
+    
   </div>
 </template>
 
 <script>
+  import LeftTabbar from "../../components/common/lefttabbar/LeftTabbar"
+  import NavBar from "../../components/common/navbar/NavBar"
+
   export default {
-    name:"category"
+    name:"category",
+    data(){
+      return {
+        category: [
+          {name : "热门推荐"},
+          {name : "手机数码"},
+          {name : "电脑办公"},
+          {name : "家用电器"},
+          {name : "美妆护肤"},
+          {name : "汽车生活"},
+          {name : "京东超市"},
+          {name : "男装"},
+          {name : "男鞋"},
+          {name : "女装"},
+          {name : "女鞋"},
+          {name : "户外运动"},
+          {name : "内衣配饰"},
+          {name : "酒水饮料"},
+          {name : "家具家装"},
+          {name : "家具出具"},
+          {name : "箱包手袋"},
+          {name : "钟表珠宝"},
+          {name : ""},
+        ]
+      }
+    },
+    components: {
+      LeftTabbar,
+      NavBar
+    }
   }
 </script>
 
 <style scoped>
-
+.categroy-nav{
+  background-color: #ff8198;
+  color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 100;
+}
 </style>
