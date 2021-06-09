a57ed86d1e8f39bbc9da28d71ab834123750fb7a
dangc <932297885@qq.com>
2021-06-09 17:47:57 +0800

    添加一些别名

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ module.exports = {
     resolve: {
       alias: {
         'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
       }
     } 
   },
