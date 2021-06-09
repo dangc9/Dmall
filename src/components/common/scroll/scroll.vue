fa96b03d840fb6be13c51ed13bcb3b3948318355
dangc <932297885@qq.com>
2021-06-09 17:45:40 +0800

    安装better-scroll和组件封装

---
+++ src/components/common/scroll/scroll.vue
@@ -0,0 +1,28 @@
+<template>
+  <div ref="wrapper">
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import scroll from 'better-scroll'
+export default {
+  name : 'scroll',
+  data(){
+    return {
+      scroll: null,
+    }
+  },
+  mounted () {
+    // 直接用$attrs获取父组件的参数，测试的时候用props在mounted拿不到。
+    // console.log(this.$attrs.options);
+    this.scroll = new scroll(this.$refs.wrapper,this.$attrs.options)  
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
